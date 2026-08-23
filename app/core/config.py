acf0078a4f6242344428198d405a77fa51cab14b
Mrutyunjaya Senapati <mrutyunjayasenapati007@gmail.com>
2026-08-23 23:40:40 +0530

    fix: import Optional in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Optional, Union
 from pydantic import AnyHttpUrl, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
